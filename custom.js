bfa6e6aa415886f6f42ac2b54c4b9110b083ea8d
yykyeong <yykyeong@naver.com>
2023-06-16 14:18:31 +0900

    07. review 섹션

--- custom.js
+++ custom.js
@@ -9,6 +9,13 @@ $(function(){
     speed: 1000, // 자동 재생 트랜지션 시간
     pauseOnHover: false // 마우스 오버될 때 멈춤 사용안함
   });
+    /* Slick-Slider - review */ 
+    $('.review-slider').slick({
+      infinite: true, // 무한반복
+      dots: true, // 동그라미 네비게이션 사용안함
+      autoplay: true, // 자동 재생
+      autoplaySpeed: 2000, // 자동 재생 시 머무는 시간
+    });
 
   /* TypeIt - Welcome */
   $('#typing').typeIt({
